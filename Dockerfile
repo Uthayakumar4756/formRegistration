0eb8249eb63367a101bfc27c2ec23457bea92013
Uthayakumar4756 <uthayakumar.venkattachalam@gmail.com>
2025-12-31 11:20:54 +0530

    Initial Spring Boot project

---
+++ Dockerfile
@@ -0,0 +1,14 @@
+# Use Java 17
+FROM eclipse-temurin:17-jdk-jammy
+
+# Set app directory
+WORKDIR /app
+
+# Copy jar file
+COPY target/demo-0.0.1-SNAPSHOT.jar app.jar
+
+# Expose port
+EXPOSE 8080
+
+# Run application
+ENTRYPOINT ["java","-jar","app.jar"]
---
+++ src/main/java/com/sample/project/Controller.java
@@ -0,0 +1,16 @@
+package com.sample.project;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+public class Controller {
+
+	@GetMapping("/main")
+	public String getapp() {
+		
+		return "hi welcome to main page";
+	}
+}
